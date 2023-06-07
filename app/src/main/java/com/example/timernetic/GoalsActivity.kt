b744c6e3e62c6466e37ed159a023e45e5acd35e6
ST10091241 <ST10091241@VCCONNECT.EDU.ZA>
2023-06-07 12:32:09 +0200

    TIMERNETI

--- app/src/main/java/com/example/timernetic/GoalsActivity.kt
+++ app/src/main/java/com/example/timernetic/GoalsActivity.kt
@@ -1,15 +1,21 @@
 package com.example.timernetic
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.TimePicker
-import androidx.drawerlayout.widget.DrawerLayout
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.util.*
 
 class GoalsActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
@@ -17,6 +23,8 @@ class GoalsActivity : AppCompatActivity() {
     private lateinit var timePickermin:TimePicker
     private lateinit var timePickermax: TimePicker
     private lateinit var AddGoal:Button
+    private lateinit var auth: FirebaseAuth
+    private lateinit var dataReference: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -68,6 +76,29 @@ class GoalsActivity : AppCompatActivity() {
         timePickermin= findViewById(R.id.timePickermin)
         timePickermax= findViewById(R.id.timePickermax)
         AddGoal= findViewById(R.id.AddGoal)
+        AddGoal.setOnClickListener {
+            val timePickermin =timePickermin.currentHour
+            val timePickermax =timePickermax.currentHour
+
+            auth = FirebaseAuth.getInstance()
+            val currentUser = auth.currentUser
+            if (currentUser != null) {
+                val userId = currentUser.uid
+                val dataReference = FirebaseDatabase.getInstance().reference
+                    .child("Task")
+                    .child(userId)
+
+                val newTask=dataReference.push()
+                newTask.child("Goal minimum").setValue(timePickermin)
+                newTask.child("Goal maximum").setValue(timePickermax).addOnCompleteListener { goalTask ->
+                    if (goalTask.isSuccessful) {
+                        Toast.makeText(applicationContext, "Goals added successfully", Toast.LENGTH_SHORT).show()
+
+                    } else {
+                        Toast.makeText(applicationContext, goalTask.exception?.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
